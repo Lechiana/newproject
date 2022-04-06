f53549e1196ba720dcd48ef12e27fc2e81643f6a
RATCHAROTE ANGTHONG <68987790+Lechiana@users.noreply.github.com>
2022-04-06 16:59:00 +0700

    edit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import Navbar from "./component/Navbar";
-import Detail from "./component/Detail";
 
 ReactDOM.render(
   <Provider store={store}>
